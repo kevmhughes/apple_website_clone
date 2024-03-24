d21a1805f5a6cd2ddd59fbdec01854fbad521e99
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2024-03-24 18:58:26 +0100

    Create VideoCarousel.jsx

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,37 +1,40 @@
-import { useGSAP } from '@gsap/react'
-import React from 'react'
-import gsap from 'gsap'
-import { rightImg, watchImg } from '../utils'
-import VideoCarousel from './VideoCarousel'
+import { useGSAP } from "@gsap/react";
+import React from "react";
+import gsap from "gsap";
+import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./VideoCarousel";
 
 const Highlights = () => {
   useGSAP(() => {
-    gsap.to("#title", {opacity: 1, y:0})
-    gsap.to(".link", {opacity: 1, y:0, duration: 1, stagger: 0.25})
-  },[])
+    gsap.to("#title", { opacity: 1, y: 0 });
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
+  }, []);
 
   return (
-      <section id="highlights" className='common-padding h-full w-screen overflow-hidden bg-zinc'>
-        <div className='screen-max-width'>
-          <div className='mb-12 w-full items-end justify-between md:flex'>
-          <h1 id="title" className='section-heading'>
+    <section
+      id="highlights"
+      className="common-padding h-full w-screen overflow-hidden bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end justify-between md:flex">
+          <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
-          <div className='flex flex-wrap items-end gap-5'>
-            <p className='link'>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
               Watch the film
-              <img src={watchImg} alt="watch" className='ml-2' />
+              <img src={watchImg} alt="watch" className="ml-2" />
             </p>
-            <p className='link'>
+            <p className="link">
               Watch the event
-              <img src={rightImg} alt="right" className='ml-2' />
+              <img src={rightImg} alt="right" className="ml-2" />
             </p>
           </div>
-          </div>
-          <VideoCarousel/>
         </div>
-      </section>
-  )
-}
+        <VideoCarousel />
+      </div>
+    </section>
+  );
+};
 
-export default Highlights
+export default Highlights;

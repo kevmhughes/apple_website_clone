57a1ce573193c2bae56140f16c492781b60b80c3
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2024-04-04 01:56:10 +0200

    Update Model.jsx and create ModelView.jsx

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,29 +1,95 @@
-import { useGSAP } from '@gsap/react'
-import React from 'react'
-import gsap from 'gsap'
+import { useGSAP } from "@gsap/react";
+import React, { useRef, useState } from "react";
+import gsap from "gsap";
+import ModelView from "./ModelView";
+import { yellowImg } from "../utils";
+import { models } from "../constants"
+
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
 
 const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 125 Pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
+    img: yellowImg,
+  });
 
-useGSAP(() => {
+  // camera control for the model view
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  // models
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  // rotation values
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largelRotation, setLargeRotation] = useState(0);
+
+  useGSAP(() => {
     gsap.to("#heading", {
-        y:0, opacity: 1
-    })
-})
+      y: 0,
+      opacity: 1,
+    });
+  });
 
   return (
-    <section className='common-padding'>
-        <div className='screen-max-width'>
-            <h1 id="heading" className='section-heading'>
-                Take a closer look.
-            </h1>
-            <div className='mt-5 flex flex-col items-center'>
-                <div className='relative h-[75vh] w-full overflow-hidden md:h-[90vh]'>
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look.
+        </h1>
+        <div className="mt-5 flex flex-col items-center">
+          <div className="relative h-[75vh] w-full overflow-hidden md:h-[90vh]">
+            <ModelView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotationState={setSmallRotation}
+              item={model}
+              size={size}
+            />
+            <ModelView
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={model}
+              size={size}
+            />
 
-                </div>
+            <Canvas
+              className="size-full"
+              style={{
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                overflow: "hidden",
+              }}
+              eventSource={document.getElementById("root")}
+            >
+              <View.Port />
+            </Canvas>
+          </div>
+          <div className="mx-auto w-full">
+            <p className="mb-5 text-center text-sm font-light">{model.title}</p>
+            <div className="flex-center">
+              <ul className="color-container">
+                {models.map((item, index) => (
+                <li key={index} className="mx-2 size-6 rounded-full" style={{backgroundColor: item.color[0]}} onClick={() => setModel(item)}/> ) )}
+                </ul>
             </div>
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Model
+export default Model;
---
+++ src/components/ModelView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ModelView = () => {
+  return (
+    <div>
+        Hey
+    </div>
+  )
+}
+
+export default ModelView

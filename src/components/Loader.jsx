699ea2a82514ce4fe114a40ad4547d99b419d415
kevmhughes <kevhughes24@hotmail.com>
2024-04-14 03:31:01 +0200

    Update Loaders.jsx
    Added loader from React Spinners

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,22 @@
-import { Html } from '@react-three/drei'
-import React from 'react'
+import { Html } from "@react-three/drei";
+import React from "react";
+import { Oval } from "react-loader-spinner";
 
 const Loader = () => {
   return (
     <Html>
       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-        <div className="w-[10vw] h-[10vw] rounded-full">
-          Loading...
-        </div>
+      <Oval
+  visible={true}
+  height="80"
+  width="80"
+  color="#878787"
+  secondaryColor="#a9a9a9"
+  ariaLabel="oval-loading"
+  />
       </div>
     </Html>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;

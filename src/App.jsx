96781f79d9cc2284521d87b3eab21023af81442a
kevmhughes <kevhughes24@hotmail.com>
2024-04-15 02:25:27 +0200

    Create Features.jsx and update App.jsx
    Add features section with animated videos.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+import Features from "./components/Features";
 
 import * as Sentry from "@sentry/react"; 
 
@@ -14,6 +15,7 @@ const App = () => {
     <Hero />
     <Highlights />
     <Model />
+    <Features />
   </main>
   )
 }
